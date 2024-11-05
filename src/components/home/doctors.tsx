51c2641682439fbb9671688bc80b7abc371cc6b7
Aditya Mali <adityamali2003@gmail.com>
2024-11-05 17:54:50 +0530

    Doctor update

--- src/components/home/doctors.tsx
+++ src/components/home/doctors.tsx
@@ -6,7 +6,7 @@ function Doctors() {
     <div className='flex flex-col items-center justify-center w-full'>
     <div className='flex flex-col items-center justify-center w-[96%] gap-8'>
         <div className='flex flex-col sm:grid grid-cols-1 sm:grid-cols-2 gap-4 w-full mt-10'>
-          <Image src="/images/vitthalmali.png" alt="Dr Vitthal Mali" height={300} width={392} />
+          <Image src="/images/doctors/vitthalmali.jpeg" alt="Dr Vitthal Mali" height={300} width={392} className='rounded-3xl aspect-square h-96' />
           <div className='md:w-[75%] flex flex-col justify-center'>
             <h2 className='text-2xl font-bold impact'><strong>Dr. Vitthal D. Mali</strong></h2>
             <span className='text-sm font-bold'>MD. PED, MBBS</span>
@@ -15,12 +15,12 @@ function Doctors() {
         </div>
         <div className='flex flex-col-reverse md:grid lg:grid-cols-2 gap-4 w-full mt-10'>
           <div className='md:w-[75%] flex flex-col justify-center md:ml-[20%]'>
-            <h2 className='text-2xl font-bold impact'><strong>Dr. Swati V. Mali</strong></h2>
-            <span className='text-sm font-bold'>BAMS</span>
-            <p>Director of Mali Children&apos;s Hospital</p>
+            <h2 className='text-2xl font-bold impact text-right'><strong>Dr. Swati V. Mali</strong></h2>
+            <span className='text-sm font-bold text-right'>BAMS</span>
+            <p className='text-right'>Director of Mali Children&apos;s Hospital</p>
           </div>
           <div className='flex justify-end items-center'>
-            <Image src="/images/swatimali.png" alt="Dr Swati Mali" height={300} width={392} />
+            <Image src="/images/doctors/swatimali.jpeg" alt="Dr Swati Mali" height={300} width={392} className='rounded-3xl aspect-square h-96'/>
           </div>
         </div>
     </div>
